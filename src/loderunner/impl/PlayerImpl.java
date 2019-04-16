25b6c410a69653d378183bffd51624ec9f416a42
Ning GUO <ai535463858@gmail.com>
2019-04-16 16:22:53 +0200

    add engine

---
+++ src/loderunner/impl/PlayerImpl.java
@@ -0,0 +1,27 @@
+package loderunner.impl;
+
+import loderunner.services.EngineService;
+import loderunner.services.PlayerService;
+
+public class PlayerImpl extends CharacterImpl implements PlayerService {
+
+	@Override
+	public EngineService getEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(EngineService e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
--- src/loderunner/services/EngineService.java
+++ src/loderunner/services/EngineService.java
@@ -1,5 +1,33 @@
 package loderunner.services;
 
+import java.util.Set;
+
 public interface EngineService {
 
+	/*
+	 * Observators
+	 */
+	public EnvironmentService getEnvironment();
+	
+	public PlayerService getPlayer();
+	
+	public Set<Guard> getGuards();
+
+	public Set<ItemService> getTreasures();
+	
+	public Command getNextCommand();
+	
+	
+	/**
+	 * pre : e!=null
+	 * post : getEngine() == e
+	 * @param e
+	 */
+	public void init(EditableScreenService screen);
+	
+
+	/*
+	 * Operator 
+	 */
+	public void step();
 }
